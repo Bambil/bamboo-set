44b50456d43dbdfbdd4cb994bc0d796961f46a22
Parham Alvani <parham.alvani@gmail.com>
2017-05-21 22:35:03 +0430

    Add some dummy test in entry point.

--- index.js
+++ index.js
@@ -21,7 +21,16 @@ winston.info(' * 18.20 at Sep 07 2016 7:20 IR721');
  * Initiates seneca modules
  */
 app.use('connectivity');
+app.use('configuration');
 
 /**
  * Seneca is ready
  */
+app.act({role: 'configuration', action: 'request', data: {
+  type: 'lamp',
+  device: '1',
+  settings: [
+    {name: 'on', value: true}
+  ],
+  agent: '42f6a151-92bb-552d-ba69-bf1d25def01f'
+}}, winston.info);
