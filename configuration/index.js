c98a967f920a55f714020efcc0f495c4f319cfa4
Parham Alvani <parham.alvani@gmail.com>
2017-05-21 23:18:05 +0430

    Configuration module was improved.

--- configuration/index.js
+++ configuration/index.js
@@ -1,10 +1,17 @@
 'use strict';
 
-const winston = require('winston');
+const schema = require('./request-schema.json');
+const ajv = require('ajv')();
+
+const validate = ajv.compile(schema);
 
 module.exports = function configuration() {
   this.add({role: 'configuration', action: 'request'}, (msg, respond) => {
-    winston.log(`request configuration ${msg}`);
+
+    if (!validate(msg.data)) {
+      return respond(validate.errors);
+    }
+
     this.act({role: 'connectivity', action: 'send', data: msg.data,
              channel: 'configuration/request'}, respond);
   });
