4097468fc9451c21b7b958b9288f6ba01dfad3e4
maryam ebrahimzadeh <maryam.ebrahimzadeh1997@yahoo.com>
2017-08-08 14:32:04 +0430

    Delete pub.py

--- pub.py
+++
@@ -1,7 +0,0 @@
-import  paho.mqtt.client as mqtt
-
-client = mqtt.Client("C1")  # create a client
-broker_address = "iot.eclipse.org"  # use external broker
-client.connect(broker_address)  # connect to broker
-
-client.publish("set", "{'isdone' : True}")
