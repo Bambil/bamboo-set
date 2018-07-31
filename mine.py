6baaf3cc11b059c3c3b23cc89dd9faedd6de5bd4
Parham Alvani <parham.alvani@gmail.com>
2018-07-31 23:05:55 +0430

    feat: remove old files

--- mine.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-r = requests.put('http://0.0.0.0:5000/' ,json = {
-    "type": "lamp",
-    "agent_id": "b07882d6-5c28-597b-89f9-d250f74b0bad",
-    "device_id": "1:5",
-    "settings": {
-        "on": True
-    }},headers={'content-type':'application/json'})
