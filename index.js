f3a9b58e4d7bca9fd8110043896e9bfb728e8dc6
Parham Alvani <parham.alvani@gmail.com>
2018-09-08 22:58:15 +0430

    feat: refactor index.js

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-'use strict';
+"use strict";
 
-const winston = require('winston');
+const winston = require("winston");
 
 /**
  * Configure CLI output on the default logger
  */
 winston.cli();
 
-
 /* Seneca application */
-const app = require('seneca')();
-
+const app = require("seneca")();
 
 /**
  * I1820 Initiation
  */
-winston.info(' * 18.20 at Sep 07 2016 7:20 IR721');
+winston.info(" * 18.20 at Sep 07 2016 7:20 IR721");
 
 /**
  * Initiates seneca modules
  */
-app.use('connectivity');
-app.use('configuration');
+app.use("connectivity");
+app.use("configuration");
 
 /**
  * Seneca is ready
  */
-app.act({role: 'configuration', action: 'request', data: {
-  type: 'lamp',
-  device: '1',
-  settings: [
-    {name: 'on', value: true}
-  ],
-  agent: '42f6a151-92bb-552d-ba69-bf1d25def01f'
-}}, winston.info);
+app.act(
+  {
+    role: "configuration",
+    action: "request",
+    data: {
+      type: "lamp",
+      device: "1",
+      settings: [{ name: "on", value: true }],
+      agent: "42f6a151-92bb-552d-ba69-bf1d25def01f",
+    },
+  },
+  winston.info
+);
